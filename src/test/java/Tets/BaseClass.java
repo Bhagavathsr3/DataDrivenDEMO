93f4486ea515e36437aeef4bccc509a5b7d199eb
Bhagavathsr3 <bhagavath696@gmail.com>
2025-09-18 23:59:56 +0530

    options

--- src/test/java/Tets/BaseClass.java
+++ src/test/java/Tets/BaseClass.java
@@ -2,6 +2,7 @@ package Tets;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,6 +12,15 @@ public class BaseClass {
 	public void base() {
 		WebDriverManager.chromedriver().setup();
 	    driver = new ChromeDriver();
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--headless");                  // Run headless
+	        options.addArguments("--no-sandbox");               // Required for Linux
+	        options.addArguments("--disable-dev-shm-usage");    // Avoid shared memory issues
+	        options.addArguments("--remote-allow-origins=*");   // For Selenium 4+
+	        options.addArguments("--disable-gpu");              // Optional
+	        options.addArguments("--user-data-dir=/tmp/chrome"); // Unique temp user data dir
+
+	        driver = new ChromeDriver(options);
 	}
 	
 	public void getUrl() {
